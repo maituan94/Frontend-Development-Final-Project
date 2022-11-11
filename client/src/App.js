327140bd3bcf099058348a531468ce487e580f8b
Julian Andres Gil Santos <gilsantosjulian@gmail.com>
2022-11-10 20:00:30 -0500

    fix: Fix elements organization - add alert component

--- client/src/App.js
+++ client/src/App.js
@@ -3,14 +3,18 @@ import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-d
 import { useSelector } from 'react-redux'
 import './_global.scss'
 import { Header, Footer } from './layout'
+import Alert from './components/Alert'
 
 const App = () => {
   return (
     <Router>
       <Header />
-      <Routes>
-        {/* Add Route here */}
-      </Routes>
+      <main className='relative flex-auto'>
+        <Routes>
+          {/* Add Route here */}
+        </Routes>
+        <Alert color="teal" title="Success" message="The user has been created" />
+      </main>
       <Footer />
     </Router>
   )
--- client/src/layout/Footer.js
+++ client/src/layout/Footer.js
@@ -1,25 +1,25 @@
 import React from 'react'
 
 const Footer = () => {
-    return (
-        <div className="fixed bottom-0 w-full ">
-            <footer className="border-t-2 border-gray-100 mx-auto max-w-7xl px-4 sm:px-6 p-4 bg-white text-center md:flex md:items-center md:justify-between md:p-6">
-                <span className="text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2022 <span>Moon</span>. All Rights Reserved.
-                </span>
-                <ul className="w-fit mx-auto md:mr-0 flex flex-wrap items-center mt-3 text-sm text-gray-500 sm:mt-0">
-                    <li>
-                        <a href="/" className="mr-4 hover:underline md:mr-6 ">About</a>
-                    </li>
-                    <li>
-                        <a href="/" className="mr-4 hover:underline md:mr-6">Privacy Policy</a>
-                    </li>
-                    <li>
-                        <a href="/" className="hover:underline">Contact</a>
-                    </li>
-                </ul>
-            </footer>
-        </div>
-    )
+  return (
+    <div className="w-full ">
+      <footer className="border-t-2 border-gray-100 mx-auto max-w-7xl px-4 sm:px-6 p-4 bg-white text-center md:flex md:items-center md:justify-between md:p-6">
+        <span className="text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2022 <span>Moon</span>. All Rights Reserved.
+        </span>
+        <ul className="w-fit mx-auto md:mr-0 flex flex-wrap items-center mt-3 text-sm text-gray-500 sm:mt-0">
+          <li>
+            <a href="/" className="mr-4 hover:underline md:mr-6 ">About</a>
+          </li>
+          <li>
+            <a href="/" className="mr-4 hover:underline md:mr-6">Privacy Policy</a>
+          </li>
+          <li>
+            <a href="/" className="hover:underline">Contact</a>
+          </li>
+        </ul>
+      </footer>
+    </div>
+  )
 }
 
 export default Footer;

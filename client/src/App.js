4068c120fddd0b83be014e53ba51ab3af583ee72
Vinesh <vineshgobin@gmail.com>
2022-11-25 01:02:50 -0500

    tabs are working

--- client/src/App.js
+++ client/src/App.js
@@ -14,6 +14,7 @@ import Products from './components/Products'
 import ModalStack from './components/ModalStack'
 
 import 'tw-elements';
+import Invoices from './components/Invoices'
 
 const Main = ({ children }) => {
   return (
@@ -49,6 +50,7 @@ const App = () => {
                 <Route path="/customers" element={<Customers />} />
                 <Route path="/suppliers" element={<Suppliers />} />
                 <Route path="/products" element={<Products />} />
+                <Route path="/invoices" element={<Invoices />} />
               </Routes>
               <ModalStack />
               <Alert />
---
+++ client/src/components/Invoices/index.js
@@ -0,0 +1,56 @@
+import React from 'react';
+
+const Invoices = () => {
+  return (
+    <div>
+      <ul class="nav nav-tabs flex flex-col md:flex-row flex-wrap list-none border-b-0 pl-0 mb-4" id="tabs-tab"
+  role="tablist">
+  <li class="nav-item" role="presentation">
+    <a href="#tabs-home" class="
+      nav-link
+      block
+      font-medium
+      text-xs
+      leading-tight
+      uppercase
+      border-x-0 border-t-0 border-b-2 border-transparent
+      px-6
+      py-3
+      my-2
+      hover:border-transparent hover:bg-gray-100
+      focus:border-transparent
+      active
+    " id="tabs-home-tab" data-bs-toggle="pill" data-bs-target="#tabs-home" role="tab" aria-controls="tabs-home"
+      aria-selected="true">Sales</a>
+  </li>
+  <li class="nav-item" role="presentation">
+    <a href="#tabs-profile" class="
+      nav-link
+      block
+      font-medium
+      text-xs
+      leading-tight
+      uppercase
+      border-x-0 border-t-0 border-b-2 border-transparent
+      px-6
+      py-3
+      my-2
+      hover:border-transparent hover:bg-gray-100
+      focus:border-transparent
+    " id="tabs-profile-tab" data-bs-toggle="pill" data-bs-target="#tabs-profile" role="tab"
+      aria-controls="tabs-profile" aria-selected="false">Purchases</a>
+  </li>
+</ul>
+<div class="tab-content" id="tabs-tabContent">
+  <div class="tab-pane fade show active" id="tabs-home" role="tabpanel" aria-labelledby="tabs-home-tab">
+    Tab 1 content
+  </div>
+  <div class="tab-pane fade" id="tabs-profile" role="tabpanel" aria-labelledby="tabs-profile-tab">
+    Tab 2 content
+  </div>
+</div>
+    </div>
+  );
+}
+
+export default Invoices;
